cbaadea3eb2474868866f10566caf96249162140
Pau Vela <pau.vela.garcia@gmail.com>
2018-08-27 16:39:13 +0200

    FIBONACCI ALGORITHM

--- R_1.R
+++ R_1.R
@@ -1,1 +1,14 @@
 library(caret)
+
+install.packages("future")
+fib<- function(n){
+  fib<- vector()
+  fib[1] <- 1
+  fib[2] <- 1
+  for(i in 3:n){
+    fib[i] <- fib[i-1] + fib[i-2] 
+  }
+  return(fib)
+}
+
+fib(7)
